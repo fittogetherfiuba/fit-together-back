da0bcec4d2341fb9dbebda383064e7f1440be8e4
spagura <sebastian.pagura@gmail.com>
2025-05-22 23:26:01 -0300

    fix register

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,7 +1,7 @@
 const pool = require('../db');
+const bcrypt = require('bcrypt');
 
-
-async function registerUser (res, req) {
+async function registerUser (req, res) {
     const { email, password, username, fullname } = req.body;
     if (!email || !password || !username || !fullname) return res.status(400).json({ error: 'Faltan campos' });
 
@@ -24,5 +24,14 @@ async function registerUser (res, req) {
         }
     }
 }
+
+function getFormattedDate() {
+    const date = new Date()
+    const day = String(date.getDate()).padStart(2, '0')
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const year = date.getFullYear()
+    
+    return `${day}/${month}/${year}`
+  }
 
 module.exports = {registerUser};
