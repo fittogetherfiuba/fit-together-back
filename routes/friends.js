c26b610d5c4efca45d69b4edf84f3918c9feab58
Martin Rosas <martin.rosas@despegar.com>
2025-05-22 22:31:37 -0300

    fix login

--- routes/friends.js
+++ routes/friends.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { acceptRequest, getFriends, getRequests, sendRequest, rejectRequest, removeFriend } = require('../controllers/friendsController');
 
-router.post('/:username', getFriends)
+router.get('/:username', getFriends)
 router.get('/requests/:username', getRequests)
 router.post('/requests', sendRequest)
 router.delete('/requests', rejectRequest)
