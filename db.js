c3f153bf58a880bf24c3d9d419a8c13244ad840d
Tomás Danko <tdanko@fi.uba.ar>
2025-06-19 23:06:50 -0300

    ssl fix

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.PRODUCTION === 'yes'
+
 const pool = new Pool({
     host: process.env.PG_HOST,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     port: process.env.PG_PORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: isProduction ? { rejectUnauthorized: false} : false
 });
 
 module.exports = pool;
