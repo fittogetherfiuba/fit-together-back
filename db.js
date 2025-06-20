df81d44533036efa09526a01df1aa19d27b978c1
Tomás Danko <tdanko@fi.uba.ar>
2025-06-19 21:27:48 -0300

    update db

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     port: process.env.PG_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
