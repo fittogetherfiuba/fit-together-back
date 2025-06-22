51dcf96d95c9a67b066fc855facf2700a7d36331
spagura <sebastian.pagura@gmail.com>
2025-06-21 21:20:43 -0300

    fix google login

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -12,23 +12,32 @@ function getFormattedDate() {
 }
 
 async function registerUser (req, res) {
-    const { email, password, username, fullname, imageUrl } = req.body;
-    if (!email || !password || !username || !fullname) return res.status(400).json({ error: 'Faltan campos' });
+    const { email, password, username, fullname, imageUrl, emailVerified } = req.body;
+    if (!email || !password || !username || !fullname) {
+        return res.status(400).json({ error: 'Faltan campos' });
+    }
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const code = generateVerificationCode();
+        const code = emailVerified ? null : generateVerificationCode(); // no necesita código si ya viene verificado
 
         const image = imageUrl ? imageUrl : 'https://i.postimg.cc/K8yZ8Mpn/user-icon-white-background.png';
+        const verified = emailVerified === true;
+
         const result = await pool.query(
-            'INSERT INTO users (email, password, username, fullname, registrationDay, image_url, verified, verification_code) VALUES ($1, $2, $3, $4, $5, $6, false, $7) RETURNING id, username, email',
-            [email, hashedPassword, username, fullname, getFormattedDate(), image, code]
+            `INSERT INTO users (email, password, username, fullname, registrationDay, image_url, verified, verification_code)
+             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+             RETURNING id, username, email`,
+            [email, hashedPassword, username, fullname, getFormattedDate(), image, verified, code]
         );
 
         const user = result.rows[0];
-        await sendVerificationEmail(user.email, code);
 
-        console.log(user)
+        if (!verified) {
+            await sendVerificationEmail(user.email, code);
+        }
+
+        console.log(user);
         res.status(201).json({ username: user.username, userId: user.id });
     } catch (err) {
         if (err.code === '23505') {
