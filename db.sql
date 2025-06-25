c2aa3e7e978e39436c2db3ba710ce9bd6e22bebc
juancpuch <jrobledo@fi.uba.ar>
2025-06-25 16:05:07 -0300

    version final script

--- db.sql
+++ db.sql
@@ -178,43 +178,43 @@ INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (1, 2, 0.2),   -- Grasas
 (1, 3, 14),    -- Carbohidratos
 (1, 4, 2.4),   -- Fibras
-(1, 5, 1);     -- Sodio (mg)
+(1, 5, 0);     -- Sodio (mg)
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (2, 1, 1.1),  (2, 2, 0.3),  (2, 3, 23),  (2, 4, 2.6),  (2, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(3, 1, 31),   (3, 2, 3.6),  (3, 3, 0),   (3, 4, 0),    (3, 5, 70);
+(3, 1, 31),   (3, 2, 3.6),  (3, 3, 0),   (3, 4, 0),    (3, 5, 2);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (4, 1, 2.7),  (4, 2, 0.3),  (4, 3, 28),  (4, 4, 0.4),  (4, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(5, 1, 3.4),  (5, 2, 1),    (5, 3, 5),   (5, 4, 0),    (5, 5, 44);
+(5, 1, 3.4),  (5, 2, 1),    (5, 3, 5),   (5, 4, 0),    (5, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(6, 1, 13),   (6, 2, 3.4),  (6, 3, 41),  (6, 4, 7),    (6, 5, 400);
+(6, 1, 13),   (6, 2, 3.4),  (6, 3, 41),  (6, 4, 7),    (6, 5, 3);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(7, 1, 13),   (7, 2, 10.6), (7, 3, 1.1), (7, 4, 0),    (7, 5, 140);
+(7, 1, 13),   (7, 2, 10.6), (7, 3, 1.1), (7, 4, 0),    (7, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (8, 1, 10),   (8, 2, 4),    (8, 3, 6),   (8, 4, 0),    (8, 5, 46);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(9, 1, 26),   (9, 2, 15),   (9, 3, 0),   (9, 4, 0),    (9, 5, 72);
+(9, 1, 26),   (9, 2, 15),   (9, 3, 0),   (9, 4, 0),    (9, 5, 0);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(10, 1, 9),   (10, 2, 0.4), (10, 3, 20), (10, 4, 8),   (10, 5, 6);
+(10, 1, 9),   (10, 2, 0.4), (10, 3, 20), (10, 4, 8),   (10, 5, 0);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(11, 1, 5),   (11, 2, 1.1), (11, 3, 25), (11, 4, 1.2), (11, 5, 1);
+(11, 1, 5),   (11, 2, 1.1), (11, 3, 25), (11, 4, 1.2), (11, 5, 0);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(12, 1, 0.9), (12, 2, 0.2), (12, 3, 3.9), (12, 4, 1.2), (12, 5, 5);
+(12, 1, 0.9), (12, 2, 0.2), (12, 3, 3.9), (12, 4, 1.2), (12, 5, 0);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(13, 1, 25),  (13, 2, 33),  (13, 3, 1.3), (13, 4, 0),   (13, 5, 621);
+(13, 1, 25),  (13, 2, 33),  (13, 3, 1.3), (13, 4, 0),   (13, 5, 7);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (14, 1, 0),   (14, 2, 100), (14, 3, 0),   (14, 4, 0),   (14, 5, 0);
@@ -223,22 +223,22 @@ INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (15, 1, 17),  (15, 2, 7),   (15, 3, 66),  (15, 4, 10),  (15, 5, 2);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(16, 1, 22),  (16, 2, 12),  (16, 3, 0),   (16, 4, 0),   (16, 5, 60);
+(16, 1, 22),  (16, 2, 12),  (16, 3, 0),   (16, 4, 0),   (16, 5, 2);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(17, 1, 25.7),(17, 2, 20.8),(17, 3, 0),   (17, 4, 0),   (17, 5, 65);
+(17, 1, 25.7),(17, 2, 20.8),(17, 3, 0),   (17, 4, 0),   (17, 5, 2);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(18, 1, 1.4), (18, 2, 0.2), (18, 3, 2.9), (18, 4, 1.3), (18, 5, 28);
+(18, 1, 1.4), (18, 2, 0.2), (18, 3, 2.9), (18, 4, 1.3), (18, 5, 2);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(19, 1, 0.9), (19, 2, 0.2), (19, 3, 10),  (19, 4, 2.8), (19, 5, 69);
+(19, 1, 0.9), (19, 2, 0.2), (19, 3, 10),  (19, 4, 2.8), (19, 5, 2);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(20, 1, 2),   (20, 2, 0.1), (20, 3, 17),  (20, 4, 2.2), (20, 5, 7);
+(20, 1, 2),   (20, 2, 0.1), (20, 3, 17),  (20, 4, 2.2), (20, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(21, 1, 2.8), (21, 2, 0.4), (21, 3, 7),   (21, 4, 2.6), (21, 5, 33);
+(21, 1, 2.8), (21, 2, 0.4), (21, 3, 7),   (21, 4, 2.6), (21, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (22, 1, 0.9), (22, 2, 0.1), (22, 3, 12),  (22, 4, 2.4), (22, 5, 0);
@@ -247,25 +247,25 @@ INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (23, 1, 0.8), (23, 2, 0.3), (23, 3, 7.7), (23, 4, 2),   (23, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(24, 1, 8),   (24, 2, 3),   (24, 3, 80),  (24, 4, 5),   (24, 5, 500);
+(24, 1, 8),   (24, 2, 3),   (24, 3, 80),  (24, 4, 5),   (24, 5, 5);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(25, 1, 8),   (25, 2, 4.8), (25, 3, 1.9), (25, 4, 0.3), (25, 5, 7);
+(25, 1, 8),   (25, 2, 4.8), (25, 3, 1.9), (25, 4, 0.3), (25, 5, 0);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(26, 1, 4.9), (26, 2, 31),  (26, 3, 46),  (26, 4, 11),  (26, 5, 24);
+(26, 1, 4.9), (26, 2, 31),  (26, 3, 46),  (26, 4, 11),  (26, 5, 0);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
 (27, 1, 21),  (27, 2, 49),  (27, 3, 22),  (27, 4, 12),  (27, 5, 1);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(28, 1, 0.5), (28, 2, 81),  (28, 3, 0.1), (28, 4, 0),   (28, 5, 11);
+(28, 1, 0.5), (28, 2, 81),  (28, 3, 0.1), (28, 4, 0),   (28, 5, 2);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(29, 1, 0.7), (29, 2, 0.2), (29, 3, 8.4), (29, 4, 0.2), (29, 5, 1);
+(29, 1, 0.7), (29, 2, 0.2), (29, 3, 8.4), (29, 4, 0.2), (29, 5, 0);
 
 INSERT INTO food_nutrients (food_id, nutrient_id, amount_per_100g) VALUES
-(30, 1, 11),  (30, 2, 4.3), (30, 3, 3.4), (30, 4, 0),   (30, 5, 364);
+(30, 1, 11),  (30, 2, 4.3), (30, 3, 3.4), (30, 4, 0),   (30, 5, 2);
 
 
 CREATE TABLE water_entries (
